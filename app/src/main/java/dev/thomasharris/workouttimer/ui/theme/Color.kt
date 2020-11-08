1ebec8cfd6d0ff7a3509e3031b222a522e972cfb
Thomas Harris <teh019283@gmail.com>
2020-11-08 13:46:13 +0100

    Update colors and icon

--- app/src/main/java/dev/thomasharris/workouttimer/ui/theme/Color.kt
+++ app/src/main/java/dev/thomasharris/workouttimer/ui/theme/Color.kt
@@ -6,3 +6,8 @@ val purple200 = Color(0xFFBB86FC)
 val purple500 = Color(0xFF6200EE)
 val purple700 = Color(0xFF3700B3)
 val teal200 = Color(0xFF03DAC5)
+
+val green200 = Color(0xFFA5D6A7)
+val green500 = Color(0xFF4CAF50)
+val green700 = Color(0xFF388E3C)
+val bluegray = Color(0xFF607D8B)
